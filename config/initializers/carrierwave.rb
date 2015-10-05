7d76e0473122b16c0ab61b7842f04cfd9535646c
Haruna Togashi <salonparu@gmail.com>
2015-10-05 13:20:57 +0000

    re change carrywave configure

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,8 @@ CarrierWave.configure do |config|
       :aws_access_key_id      => ENV['HUBER_AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key  => ENV['HUBER_AWS_SECRET_ACCESS_KEY'],
       :region                 => ENV['HUBER_AWS_REGION'],
-      :path_style             => true
+      :path_style             => true,
+      :endpoint               => 'http://s3.amazonaws.com'
     }
     config.fog_directory = ENV['HUBER_AWS_S3_BUCKET']
     config.fog_authenticated_url_expiration = 60
