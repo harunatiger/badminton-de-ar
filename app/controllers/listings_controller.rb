b4387884189719561eaa77edfaac80dc5b467cee
name <salonparu@gmail.com>
2015-10-28 06:50:42 +0000

    add includes listing_detail

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -117,7 +117,7 @@ class ListingsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
-      @listing = Listing.where(id: params[:id]).first
+      @listing = Listing.where(id: params[:id]).first.includes(:listing_detail)
       return redirect_to root_path, notice: Settings.listings.error.invalid_listing_id if @listing.blank?
     end
 
