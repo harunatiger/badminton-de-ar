d7788c2115bd82d0fbe81d890c913f5d57b7d60c
kamitokusari <koala@koa-labo.com>
2015-11-09 13:55:42 +0900

    Fix migration file for add_column_to_profile

--- db/migrate/20151106154709_add_column_to_profile.rb
+++ db/migrate/20151106154709_add_column_to_profile.rb
@@ -1,5 +1,5 @@
 class AddColumnToProfile < ActiveRecord::Migration
   def change
-    add_column :profiles, :progress, :integer
+    add_column :profiles, :progress, :integer, :default => 0
   end
 end
