057a9bc07c78c6aaf51b8b4538ffa2536259a63f
haruna togashi <salonparu@gmail.com>
2015-11-13 08:29:36 +0000

    delete drop table migration file

--- db/migrate/20151113075716_drop_table_pickups.rb
+++
@@ -1,7 +0,0 @@
-class DropTablePickups < ActiveRecord::Migration
-  def change
-    drop_table :listing_pickup_areas
-    drop_table :listing_pickup_categories
-    drop_table :listing_pickup_tags
-  end
-end
