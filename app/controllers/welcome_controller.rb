5250b49be76dea95f436a9971e3ddaabe0e82ce3
kamitokusari <koala@koa-labo.com>
2015-11-16 16:21:18 +0900

    Fix Display Pickup Random

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @profiles = Profile.guides.includes(:profile_image)
-    @pickups = Pickup.find(Pickup.where.not(selected_listing: nil))
+    @pickups = Pickup.find(Pickup.where.not(selected_listing: nil).pluck(:id))
   end
 end
