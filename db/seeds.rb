14659579e63bbc459d15a80d8ef285b115dd57c1
haruna togashi <salonparu@gmail.com>
2015-12-01 09:32:12 +0000

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
 require 'csv'
 
+Category.delete_all
+Category.connection.execute("SELECT SETVAL('categories_id_seq',1,FALSE)")
 CSV.foreach('db/seeds_data/categories.csv') do |row|
   Category.create(name: row[0])
 end
 
+Language.delete_all
+Language.connection.execute("SELECT SETVAL('languages_id_seq',1,FALSE)")
 CSV.foreach('db/seeds_data/languages.csv') do |row|
   Language.create(name: row[0])
 end
