17f11e0b44e146cdc860059801553a727f652f57
haruna togashi <salonparu@gmail.com>
2015-12-01 06:47:57 +0000

    fix account edit function

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,5 +10,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
     hash[:uid] = User.create_unique_string
     super
   end
-
+  
+  protected
+  def update_resource(resource, params)
+    if params[:email]
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
 end
