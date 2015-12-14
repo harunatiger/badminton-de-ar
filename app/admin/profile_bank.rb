39ec7a16442f64913d963b8ea6dfb4a037cfade5
haruna togashi <salonparu@gmail.com>
2015-12-14 12:25:03 +0000

    fix admin/profile_bank

--- app/admin/profile_bank.rb
+++ app/admin/profile_bank.rb
@@ -1,28 +1,3 @@
 ActiveAdmin.register ProfileBank do
-  permit_params :paypal_account
-  
-  index do
-    column :id
-    column :paypal_account
-    column :created_at
-    column :updated_at
-    actions
-  end
-  
-  show do
-    attributes_table do
-      row :id
-      row :paypal_account
-      row :created_at
-      row :updated_at
-    end
-    active_admin_comments
-  end
-  
-  form do |f|
-    f.inputs do
-      f.input :paypal_account
-    end
-    f.actions
-  end
+  permit_params :user_id, :profile_id, :name, :branch_name, :account_type, :user_name, :number, :paypal_account
 end
