af7cf1ed7d6e8e9105458795d0526b8107a20424
kamitokusari <koala@koa-labo.com>
2015-12-17 11:26:25 +0900

    Change MailTo for GuideRequest

--- app/mailers/pre_mailer.rb
+++ app/mailers/pre_mailer.rb
@@ -1,10 +1,10 @@
 class PreMailer < ApplicationMailer
-  
+
   def send_pre_mail(user)
     @user = user
     @user_name  = "#{user.profile.first_name} #{user.profile.last_name}"
     mail(
-      to:      ENV["OWNER_MAIL_ADDRESS"],
+      to:      ENV["OWNER_MAIL_ADDRESS_GUIDE_REQUEST"],
       subject: Settings.mailer.pre_mail.subject
     ) do |format|
       format.text
