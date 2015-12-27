cbff095d246e16a523e1db17b705c5685c4a8cdd
haruna togashi <salonparu@gmail.com>
2015-12-27 09:14:40 +0000

    change profile identities admin view

--- app/admin/profile_identity.rb
+++ app/admin/profile_identity.rb
@@ -1,13 +1,29 @@
 ActiveAdmin.register ProfileIdentity do
   
   permit_params :user_id, :profile_id, :image, :caption, :authorized
+  
+  index do
+    column :id
+    column :user_id do |obj|
+      link_to obj.id, profile_path(obj.profile_id), :target => ["_blank"]
+    end
+    column :first_name do |obj|
+      obj.profile.first_name
+    end
+    column :image
+    column :authorized
+    column :created_at
+    column :updated_at
+    actions
+  end
+  
   show do
     attributes_table do
       row :id
       row :user_id
       row :profile_id
       row :image do |obj|
-        image_tag(obj.image.url(:thumb))
+        link_to image_tag(obj.image.url(:thumb)), obj.image.url, :target => ["_blank"]
       end
       row :caption
       row :authorized
