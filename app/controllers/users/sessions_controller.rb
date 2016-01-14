d46e40128d4eeecd86a1d43a034e3ea737038956
kamitokusari <koala@koa-labo.com>
2016-01-14 12:59:18 +0900

    Add slider image and Fix message link

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,21 +2,21 @@ class Users::SessionsController < Devise::SessionsController
   def new
     super
   end
- 
+
   def create
     super
   end
- 
+
   def destroy
     super
   end
-  
+
   def after_sign_in_path_for(resource)
-    if (session[:previous_url] == root_path)
-      super
-    else
+    #if (session[:previous_url] == root_path)
+    #  super
+    #else
       session[:previous_url] || root_path
-    end
+    #end
   end
 end
 
