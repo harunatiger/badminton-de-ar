6bd87d44593899b43781d5aad67c1f7b24172249
haruna togashi <salonparu@gmail.com>
2016-01-15 08:34:38 +0000

    fix redirect page after facebook login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,11 +12,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
   
   def after_sign_in_path_for(resource)
-    if (session[:previous_url] == root_path)
-      super
-    else
-      session[:previous_url] || root_path
-    end
+    session[:previous_url] || root_path
   end
  
 end
