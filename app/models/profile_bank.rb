c93767eb4bf2d81dff9b7fd15acc9cd745c2d64a
haruna togashi <salonparu@gmail.com>
2016-01-27 05:59:00 +0000

    add validation for profile_bank

--- app/models/profile_bank.rb
+++ app/models/profile_bank.rb
@@ -30,4 +30,6 @@ class ProfileBank < ActiveRecord::Base
   #validates :user_name, presence: true
   #validates :number, presence: true
   validates :profile_id, uniqueness: true
+  validates :user_name, format: {with: /^[\p{katakana}ー－[[:blank:]][ｧ-ﾝﾞﾟ]]+$/, :multiline => true, message: :katakana_error}
+  validates :number, length: {maximum: Settings.profile_banks.number_length.to_i, message: I18n.t('errors.messages.too_long', length: Settings.profile_banks.number_length)}
 end
