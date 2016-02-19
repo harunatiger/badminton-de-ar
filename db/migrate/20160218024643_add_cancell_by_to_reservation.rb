1fb04228bb72f7b4b28252ce5d4ae68d881d0ae9
kamitokusari <koala@koa-labo.com>
2016-02-19 20:21:46 +0900

    Fix migration file for cancel_by

--- db/migrate/20160218024643_add_cancell_by_to_reservation.rb
+++ db/migrate/20160218024643_add_cancell_by_to_reservation.rb
@@ -4,15 +4,15 @@ class AddCancellByToReservation < ActiveRecord::Migration
     Payment.all.each do |p|
       r = p.reservation
       if p.payment_status  == 'Refunded'
-        if r.refund_rate == Settings.payment.refunds.before_weeks_rate && r.progress == 6
+        if r.refund_rate == 100 && r.canceled_after_accepted?
           cancel_by = 2
-        elsif r.refund_rate == Settings.payment.refunds.before_days_rate && r.progress == 6
+        elsif r.refund_rate == 50 && r.canceled_after_accepted?
           cancel_by = 3
         else
           cancel_by = 4
         end
       else
-        if r.refund_rate == 0 && r.progress == 6
+        if r.refund_rate == 0 && r.canceled_after_accepted?
           cancel_by = 4
         else
           cancel_by = 0
