663751b1c868ceba9d01f605d161e2263ea461a8
kamitokusari <koala@koa-labo.com>
2016-03-11 08:34:43 +0900

    fix allow 'https'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -574,7 +574,7 @@ module ApplicationHelper
   end
 
   def text_url_to_link(text)
-    URI.extract(text, ['http']).uniq.each do |url|
+    URI.extract(text, ['http','https']).uniq.each do |url|
       sub_text = ""
       sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
       text.gsub!(url, sub_text)
