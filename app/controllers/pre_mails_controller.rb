3780b1be3a02c670c06c7efee957757883079a84
haruna togashi <salonparu@gmail.com>
2016-03-29 08:41:23 +0000

    fix top guide request text

--- app/controllers/pre_mails_controller.rb
+++ app/controllers/pre_mails_controller.rb
@@ -39,9 +39,9 @@ class PreMailsController < ApplicationController
         if @pre_mail.save
           PreMailer.send_pre_mail(@pre_mail).deliver_now!
           PreMailer.send_pre_mail_to_user(@pre_mail).deliver_now!
-          format.html { redirect_to root_path, notice: Settings.pre_mail.save.success }
+          format.js { @status = 'success' }
         else
-          format.html { redirect_to root_path, notice: Settings.pre_mail.save.failure }
+          format.js { @status = 'failuer' }
         end
       else
         format.html { redirect_to root_path, notice: Settings.pre_mail.save.failure }
