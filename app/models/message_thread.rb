4def30dafd299105c56b676d686458220830f1eb
Gaku Hirabayashi <morinosuke5@gmail.com>
2016-04-07 20:29:29 +0900

    gahhhh

--- app/models/message_thread.rb
+++ app/models/message_thread.rb
@@ -24,7 +24,6 @@ class MessageThread < ActiveRecord::Base
   has_many :users, through: :message_thread_users, dependent: :destroy
 
   attr_accessor :reservation_progress
-  self.inheritance_column = :_type_disabled
   
   scope :order_by_updated_at_desc, -> { order('updated_at') }
   scope :noreply_push_mail, -> { where(noticemail_sended: false, reply_from_host: false, first_message: true) }
