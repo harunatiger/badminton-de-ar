d8b1488aa308dcd4b2de0140ea3c671fab504fbb
name <salonparu@gmail.com>
2016-08-30 10:34:06 +0000

    change method of getting default currency

--- app/controllers/concerns/currency.rb
+++ app/controllers/concerns/currency.rb
@@ -3,22 +3,26 @@ module Currency
     ['AUD','CAD','CZK','DKK','EUR','HKD','HUF','ILS','JPY','MXN','NOK','NZD','PHP','PLN','GBP','RUB','SGD','SEK','CHF','TWD','THB','USD']
   end
   
-  def self.locale_and_currency_hash
-    {AU: 'AUD', CA: 'CAD', CZ: 'CZK', DK: 'DKK', EU: 'EUR', HK: 'HKD', HU: 'HUF', IL: 'ILS', JP: 'JPY', MX: 'MXN', NO: 'NOK', NZ: 'NZD', PH: 'PHP', PL: 'PLN', GB: 'GBP', RU: 'RUB', SG: 'SGD', SE: 'SEK', CH: 'CHF', TW: 'TWD', TH: 'THB', US: 'USD'}
+  def self.language_and_currency_hash
+    {'en-AU' => 'AUD', 'en-CA' => 'CAD', 'fr-CA' => 'CAD', 'cs' => 'CZK', 'da' => 'DKK', "ga" => 'EUR', "it" => 'EUR', "et" => 'EUR', "de" => 'EUR', "nl" => 'EUR', "el" => 'EUR', "es" => 'EUR', "sk" => 'EUR', "sl" => 'EUR', "fi" => 'EUR', "fr" => 'EUR', "pt" => 'EUR', "mt" => 'EUR', "lv" => 'EUR', "lt" => 'EUR', "zh" => 'HKD', "zh-HK" => 'HKD', "hu" => 'HUF', "he" => 'ILS', "ar" => 'ILS', 'ja' => 'JPY', "es-MX" => 'MXN', "no" => 'NOK', "en-NZ" => 'NZD', "fil" => 'PHP', "pl" => 'PLN', "en-GB" => 'GBP', "ru" => 'RUB', "sv" => 'SEK', "th" => 'THB', "en-US" => 'USD', "en" => 'USD'}
   end
   
   def self.currency_code_and_sign_hash
     { 'AUD' => '$', 'CAD' => '$', 'CZK' => 'Kč', 'DKK' => 'kr', 'EUR' => '€', 'HKD' => 'HK$', 'HUF' => 'Ft', 'ILS' => '₪', 'JPY' => '¥', 'MXN' => '$', 'NOK' => 'kr', 'NZD' => '$', 'PHP' => '₱', 'PLN' => 'zł', 'GBP' => '£', 'RUB' => '₽', 'SGD' => '$', 'SEK' => 'kr', 'CHF' => 'S₣', 'TWD' => 'NT$', 'THB' => '฿', 'USD' => '$'}
   end
   
-  def self.local_to_currency_code(locale)
-    return 'JPY' if locale.blank? or self.locale_and_currency_hash[locale].blank?
-    self.locale_and_currency_hash[locale]
+  def self.language_to_currency_code(language)
+    return 'JPY' if language.blank?
+    language_and_currency_hash[language]
   end
   
   def self.get_rate(to_currency_code)
     exchange = PaypalAdaptive::Request.new
     response = exchange.convert_currency({requestEnvelope: {}, baseAmountList: [currency: {amount: 100, code: 'JPY'}], convertToCurrencyList: [{currencyCode: to_currency_code}]})
     p rate = response['estimatedAmountTable']['currencyConversionList'][0]['currencyList']['currency'][0]['amount'].to_f / 100
+  end
+  
+  def self.available_locales
+      ['en-AU', 'en-CA', 'fr-CA', 'cs', 'da', "ga", "it", "et", "de", "nl", "el", "es", "sk", "sl", "fi", "fr", "pt", "mt", "lv", "lt", "zh", "zh-HK", "hu", "he", "ar", "ja", "es-MX", "no", "en-NZ", "fil", "pl", "en-GB",'en', "ru", "sv", "th", "en-US"]
   end
 end
