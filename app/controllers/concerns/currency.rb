946bc9176514b7a7e2c1ec58f15a5d6c915089c9
name <salonparu@gmail.com>
2016-10-06 04:19:08 +0000

    change available currency

--- app/controllers/concerns/currency.rb
+++ app/controllers/concerns/currency.rb
@@ -1,10 +1,12 @@
 module Currency
   def self.currency_list
-    ['AUD','CAD','CZK','DKK','EUR','HKD','HUF','ILS','JPY','MXN','NOK','NZD','PHP','PLN','GBP','RUB','SGD','SEK','CHF','TWD','THB','USD']
+    #['AUD','CAD','CZK','DKK','EUR','HKD','HUF','ILS','JPY','MXN','NOK','NZD','PHP','PLN','GBP','RUB','SGD','SEK','CHF','TWD','THB','USD']
+    ['EUR','JPY','USD']
   end
   
   def self.language_and_currency_hash
-    {'en-AU' => 'AUD', 'en-CA' => 'CAD', 'fr-CA' => 'CAD', 'cs' => 'CZK', 'da' => 'DKK', "ga" => 'EUR', "it" => 'EUR', "et" => 'EUR', "de" => 'EUR', "nl" => 'EUR', "el" => 'EUR', "es" => 'EUR', "sk" => 'EUR', "sl" => 'EUR', "fi" => 'EUR', "fr" => 'EUR', "pt" => 'EUR', "mt" => 'EUR', "lv" => 'EUR', "lt" => 'EUR', "zh" => 'HKD', "zh-HK" => 'HKD', "hu" => 'HUF', "he" => 'ILS', "ar" => 'ILS', 'ja' => 'JPY', "es-MX" => 'MXN', "no" => 'NOK', "en-NZ" => 'NZD', "fil" => 'PHP', "pl" => 'PLN', "en-GB" => 'GBP', "ru" => 'RUB', "sv" => 'SEK', "th" => 'THB', "en-US" => 'USD', "en" => 'USD'}
+    #{'en-AU' => 'AUD', 'en-CA' => 'CAD', 'fr-CA' => 'CAD', 'cs' => 'CZK', 'da' => 'DKK', "ga" => 'EUR', "it" => 'EUR', "et" => 'EUR', "de" => 'EUR', "nl" => 'EUR', "el" => 'EUR', "es" => 'EUR', "sk" => 'EUR', "sl" => 'EUR', "fi" => 'EUR', "fr" => 'EUR', "pt" => 'EUR', "mt" => 'EUR', "lv" => 'EUR', "lt" => 'EUR', "zh" => 'HKD', "zh-HK" => 'HKD', "hu" => 'HUF', "he" => 'ILS', "ar" => 'ILS', 'ja' => 'JPY', "es-MX" => 'MXN', "no" => 'NOK', "en-NZ" => 'NZD', "fil" => 'PHP', "pl" => 'PLN', "en-GB" => 'GBP', "ru" => 'RUB', "sv" => 'SEK', "th" => 'THB', "en-US" => 'USD', "en" => 'USD'}
+    {'en-AU' => 'USD', 'en-CA' => 'USD', 'fr-CA' => 'USD', 'cs' => 'USD', 'da' => 'USD', "ga" => 'EUR', "it" => 'EUR', "et" => 'EUR', "de" => 'EUR', "nl" => 'EUR', "el" => 'EUR', "es" => 'EUR', "sk" => 'EUR', "sl" => 'EUR', "fi" => 'EUR', "fr" => 'EUR', "pt" => 'EUR', "mt" => 'EUR', "lv" => 'EUR', "lt" => 'EUR', "zh" => 'USD', "zh-HK" => 'USD', "hu" => 'USD', "he" => 'USD', "ar" => 'USD', 'ja' => 'JPY', "es-MX" => 'USD', "no" => 'USD', "en-NZ" => 'USD', "fil" => 'USD', "pl" => 'USD', "en-GB" => 'USD', "ru" => 'USD', "sv" => 'USD', "th" => 'USD', "en-US" => 'USD', "en" => 'USD'}
   end
   
   def self.currency_code_and_sign_hash
