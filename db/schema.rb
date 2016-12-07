ffe6f4aa3f2c48afbf06a237da4c97fa0b8e1f04
harunatiger <salonparu@gmail.com>
2016-12-07 13:01:14 +0000

    add planning for you radio

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117113720) do
+ActiveRecord::Schema.define(version: 20161207110150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -749,6 +749,7 @@ ActiveRecord::Schema.define(version: 20161117113720) do
     t.string   "other_address",        default: ""
     t.datetime "soft_destroyed_at"
     t.text     "free_field",           default: ""
+    t.boolean  "planning_for_you"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
@@ -971,12 +972,12 @@ ActiveRecord::Schema.define(version: 20161117113720) do
   add_index "user_campaigns", ["user_id"], name: "index_user_campaigns_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                   default: "",    null: false
+    t.string   "encrypted_password",      default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",           default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -985,22 +986,25 @@ ActiveRecord::Schema.define(version: 20161117113720) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,     null: false
+    t.integer  "failed_attempts",         default: 0,     null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "uid",                    default: "",    null: false
-    t.string   "provider",               default: "",    null: false
+    t.string   "uid",                     default: "",    null: false
+    t.string   "provider",                default: "",    null: false
     t.string   "username"
     t.datetime "soft_destroyed_at"
-    t.string   "email_before_closed",    default: ""
-    t.text     "reason",                 default: ""
-    t.integer  "user_type",              default: 0
+    t.string   "email_before_closed",     default: ""
+    t.text     "reason",                  default: ""
+    t.integer  "user_type",               default: 0
     t.date     "last_access_date"
     t.datetime "admin_closed_at"
     t.text     "remarks"
-    t.boolean  "star_guide",             default: false
+    t.boolean  "star_guide",              default: false
+    t.uuid     "uuid"
+    t.string   "access_token"
+    t.string   "access_token_expires_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
