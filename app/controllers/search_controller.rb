705b5f29da5d5f9bfd8b637cd0ef355116456694
harunatiger <salonparu@gmail.com>
2016-12-28 12:27:23 +0000

    add map search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,9 @@ class SearchController < CommonSearchController
   def search
     basic_search
     gon.pickup_areas = PickupArea.list_for_gon
+    if request.xhr?
+      return render json: { html: render_to_string(partial: 'search/result_block', locals: { results: @results, conditions: @conditions, new_listings: @new_listings }), locations: gon.locations}
+    end
   end
   
   def search_result
