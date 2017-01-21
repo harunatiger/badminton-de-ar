4d698634c3563b52977b2db53ef0851807919aa0
harunatiger <salonparu@gmail.com>
2017-01-21 07:40:28 +0000

    change

--- app/controllers/api/v1/counts_controller.rb
+++ app/controllers/api/v1/counts_controller.rb
@@ -3,10 +3,11 @@ module Api
     class CountsController < Api::V1::BaseApiController
       def add
         count = Count.find_by_player_id(params[:player_id].to_i)
+        num = params[:count].to_i
         if count.blank?
           count = Count.create(player_id: params[:player_id].to_i, count: 0)
         end
-        count.count += 1
+        count.count += num
         count.save
         return render json: { }, status: :ok
       end
